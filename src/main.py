de72d30173512d8086e9c86b7037ef75544e587f
root <root@localhost>
2022-05-30 18:32:38 +0100

    auto publish 1653931958609

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-basket = ["Apple", "Orange", "Strawberry", "Banana"]
-for fruit in basket:
-    message = "Fresh " + fruit
-    print(message)
+print (3/4)
+print (0.42)
+print (6 * 9.0)
+print (4 + 1.65)
+print (1+2+3+4.0+5)
+print (2**5)
+print (9 ** (1/2))
+print (8**(1/3))
+print (20//6)
+print (10//4)
+print (7%(5//2))
+print (1+4*3)
+print ((3**2)//2)
